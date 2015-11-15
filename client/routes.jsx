ce97894041a9592ee175968778906885c2f90632
JD Wong <jdmswong@gmail.com>
2015-11-15 08:06:59 -0800

    players join lobbies they enter

--- client/routes.jsx
+++ client/routes.jsx
@@ -11,7 +11,22 @@ const routes = (
 			<Route path="lobbies" component={LobbiesView}></Route>
 			<Route path="newLobby" component={NewLobby}></Route>
 			<Route path="game" component={BoxClick}></Route>
-			<Route path="lobby/:lobbyId" component={Lobby}></Route>
+			<Route
+				path="lobby/:lobbyId"
+				component={Lobby}
+				onEnter={function(nextState, replaceState){
+					Meteor.call("joinLobby", nextState.params.lobbyId, function(error, result){
+						if(error){
+							console.log("error", error);
+						}
+						if(typeof(result) != "undefined"){
+							if(result == false){
+								// the user has entered a full room, may want to redirect here
+							}
+						}
+					});
+				}}
+			></Route>
 		</Route>
 		<Route path="*" component={Welcome}/>
 	</Route>
--- server/userMethods.js
+++ server/userMethods.js
@@ -19,13 +19,15 @@ Meteor.methods({
 
 		function constructSlots(size){
 			const emptySlot = { id: undefined };
-			let result = [{
-				id: Meteor.userId(),
-				name: Meteor.user().username,
-				readyStatus: false
-			}];
-			for( let i=1; i<size; i++){
-				result.push(Object.assign({}, emptySlot));
+			let result = {
+				1: {
+					id: Meteor.userId(),
+					name: Meteor.user().username,
+					readyStatus: false
+				}
+			};
+			for( let i=2; i<=size; i++){
+				result[i] = Object.assign({}, emptySlot);
 			}
 			return result;
 		};
@@ -37,5 +39,45 @@ Meteor.methods({
 			playerCount: 1
 		}) )
 			return true;
+	},
+
+	joinLobby(lobbyId){
+
+		Meteor.call('requireUser');
+
+		check(lobbyId, String);
+
+		const lobby = Lobbies.findOne(lobbyId);
+
+		const freeSlot = (function findSlot(){
+			for( let i=1; i <= lobby.playerCapacity; i++ ){
+				if( lobby.slots[i].id == null ){
+					return i;
+				}
+			}
+			return false;
+		})();
+
+		if( freeSlot === false )
+			return false;
+
+		function filledSlot(playerId, playerName){
+			return {
+				id: playerId,
+				name: playerName,
+				readyStatus: false
+			};
+		}
+
+		//return
+			Meteor.users.update(Meteor.userId(),{$set: {"profile.currentLobby": lobbyId}});
+			//&&
+			Lobbies.update(lobbyId,
+				{$set: {
+					["slots."+freeSlot]: filledSlot(Meteor.userId(), Meteor.user().username)
+				}}
+			);
+
+
 	}
 });
