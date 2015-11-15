69744fd43658094d6f25b46a7bb0b7a59fc6c626
JD Wong <jdmswong@gmail.com>
2015-11-14 21:43:49 -0800

    adding player slot select

--- client/components/newLobby/NewLobbyForm.jsx
+++ client/components/newLobby/NewLobbyForm.jsx
@@ -2,10 +2,58 @@ NewLobbyForm = React.createClass({
 
 	mixins: [ReactMeteorData],
 
+	getInitialState() {
+		return {
+			selectedGame: undefined
+		};
+	},
+
 	getMeteorData() {
 		return {
 			games: Games.find().fetch()
 		};
+	},
+
+	getGames(){
+		let allGames = this.data.games.map(function(game){
+			return ( <option key={game._id} value={game._id}>{game.name}</option> );
+		});
+
+		return (
+			<select className="form-control" id="selectGame" onChange={this.handleNewSelection}>
+				<option value="">Select ...</option>
+				{allGames}
+			</select>
+		);
+
+	},
+
+	getOpenSlots(){
+		if( !this.state.selectedGame ) return ("");
+		let allOptions = [];
+		let game = this.state.selectedGame;
+
+		for (let i = game.minPlayers; i <= game.maxPlayers; i++) {
+			allOptions.push(<option key={i} value={i}>{i}</option>);
+		}
+
+		return (
+			<div className="form-group">
+				<label for="openSlots">How many players?: </label>
+				<select className="form-control" id="openSlots">
+					{allOptions}
+				</select>
+			</div>
+		);
+	},
+
+	// Triggers when user selects a game
+	handleNewSelection(event) {
+
+		this.setState({
+			selectedGame: event.target.value.length > 0 ?
+				Games.findOne(event.target.value) : undefined
+		});
 	},
 
 	render() {
@@ -21,18 +69,14 @@ NewLobbyForm = React.createClass({
 
 				<div className="form-group">
 					<label for="selectGame">Select game: </label>
-					<select className="form-control" id="selectGame">
-						<option>Select ...</option>
-					</select>
+					{this.getGames()}
 				</div>
 
-				<div className="form-group">
-					<label for="openSlots">How many players?: </label>
-					<select className="form-control" id="openSlots">
-						<option>1</option>
-					</select>
-				</div>
-
+				{this.state.selectedGame ?
+					this.getOpenSlots()
+					:
+					""
+				}
 				<button type="submit" className="btn btn-default">Submit</button>
 			</form>
 
